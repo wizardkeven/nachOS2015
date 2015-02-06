89c0056585d206f81c1fa20bd19d9ee568db2aac
wizardkeven <wizardkeven@live.com>
2015-02-06 10:13:10 +0100

    Add synchconsole.h and synchconsole.cc. Complete one line

--- userprog/synchconsole.cc
+++ userprog/synchconsole.cc
@@ -13,7 +13,7 @@ SynchConsole::SynchConsole(char *readFile, char *writeFile)
 {
 	readAvail = new Semaphore("read avail", 0);
 	writeDone = new Semaphore("write done", 0);
-	//console = ...
+	console = new Console(readFile, writeFile, ReadAvail,WriteDone,0);
 }
 
 SynchConsole::~SynchConsole()
